4aba69d390561f38366b2b9180b12a6402ba20ab
RONIT CHATTOPADHYAY <ronit@yourdost.com>
2019-08-27 23:13:56 +0530

    first commit

---
+++ rabbiting-demo/src/main/java/io/qkr/rabbitingdemo/RabbitingDemoApplication.java
@@ -0,0 +1,13 @@
+package io.qkr.rabbitingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitingDemoApplication.class, args);
+	}
+
+}
